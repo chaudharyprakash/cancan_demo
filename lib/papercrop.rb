6296ab6f2f62a94228efdc7df9764dfc0a6d7296
chaudharyprakash1992@gmail.com <kushal.mistry@bacancytechnology.com>
2016-02-16 10:47:29 +0530

    add functionality for image croping

---
+++ lib/papercrop.rb
@@ -0,0 +1,6 @@
+require 'papercrop/engine'
+require 'papercrop/reg_exp'
+require 'papercrop/model_extension'
+require 'papercrop/helpers'
+require 'papercrop/logger'
+require 'paperclip_processors/papercrop'
